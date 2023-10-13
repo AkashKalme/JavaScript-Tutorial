b39822dfe7834de83b038683be601a87e1d3027c
Akash Kalme <kalmeakash@gmail.com>
2023-10-13 23:33:54 +0530

    13th Oct

---
+++ 4.js
@@ -0,0 +1,31 @@
+// Primitives and Objects in JS
+// Primitives in JS
+// Null
+// Number
+// Symbol
+// String
+// Boolean
+// Bigint
+// Undefined
+
+let a = null;
+let b = 45;
+let c = true;
+let d = BigInt("567")
+let e = "Akash"
+let f = Symbol("I am a symbol")
+let g = undefined
+
+console.log(a,b,c,d,e,f,g);
+console.log(typeof d);
+
+// Objects in JS
+const item = {
+    "Akash": true,
+    "Harry": false,
+    "Ram": 101,
+    "Rohan": undefined
+}
+
+console.log(item["Ram"]);
+console.log(item["Tejas"]);
